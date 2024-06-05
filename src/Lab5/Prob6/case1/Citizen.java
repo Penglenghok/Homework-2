369aef254834a163f66d7ed7ff663905fe0e1379
ThezThaw <97180604+ThezThaw@users.noreply.github.com>
2024-06-04 20:05:41 -0500

    Correct param name

--- src/Lab5/Prob6/case1/Citizen.java
+++ src/Lab5/Prob6/case1/Citizen.java
@@ -3,8 +3,8 @@ package Lab5.Prob6.case1;
 public class Citizen extends Person{
     private String gender;
 
-    public Citizen(String country, String gender) {
-        super(country);
+    public Citizen(String citizen, String gender) {
+        super(citizen);
         this.gender = gender;
     }
 }
