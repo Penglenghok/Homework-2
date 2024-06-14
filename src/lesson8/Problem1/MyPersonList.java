56206b0cbdd21c64412ba552ee58b3c4b588bacc
AnhTuanHa <tuanha17897@gmail.com>
2024-06-13 23:02:11 -0500

    fix error

--- src/lesson8/Problem1/MyPersonList.java
+++ src/lesson8/Problem1/MyPersonList.java
@@ -5,71 +5,77 @@ import java.util.Arrays;
 // Common task : Make sure to check your code should not throw any run time exception
 public class MyPersonList {
     private final int INITIAL_LENGTH = 3;
-    private Person[] PersonArray;
+    private Person[] personArray;
     private int size;
 
     // Default Constructor
     public MyPersonList() {
-        PersonArray = new Person[INITIAL_LENGTH];
+        personArray = new Person[INITIAL_LENGTH];
         size = 0;
     }
 
     // 1. Implement method to Add as last element, make sure you have enough room else call resize()
     public void add(Person s) {
-        if (s.equals(null))
-            return;
-        if (size == PersonArray.length)
-            resize();
-        PersonArray[size] = s;
-        size++;
+        if (s != null){
+            if (size == personArray.length) {
+                resize();
+            }
+            personArray[size] = s;
+            size++;
+        }
     }
 
     // 2. get person information by passing index
     public Person get(int i) {
-        if (i < 0 || i > size - 1) {
+        if (i < 0 || i >= size) {
             return null;
         }
-        return PersonArray[i];
+
+        return personArray[i];
     }
 
     // 3. Find the Person object using lastname
     public boolean find(String lastName) {
-        for (int i = 0; i < PersonArray.length; i++) {
-            if (PersonArray[i] != null && PersonArray[i].getLast().equals(lastName))
-                return true;
+        if (lastName != null){
+            for(Person p : personArray){
+                if (p.getLast().equals(lastName)){
+                    return true;
+                }
+            }
         }
         return false;
     }
 
     // 4. Remove the person object by passing its lastname
     public boolean remove(String lastName) {
-        for (int i = 0; i < PersonArray.length; i++) {
-            if (PersonArray[i] != null && PersonArray[i].getLast().equals(lastName)) {
-                PersonArray[i] = null;
-                size--;
-                return true;
+        if (lastName != null){
+            boolean found = false;
+            for(int i = 0; i < size; i++){
+                if (personArray[i].getLast().equals(lastName)){
+                    found = true;
+                }
+                if (found){
+                    System.arraycopy(personArray, i + 1, personArray, i, --size);
+                    return true;
+                }
             }
+
         }
         return false;
     }
-
     // 5. Resizing the list
     private void resize() {
-        int len = PersonArray.length;
-        int newlen = 2 * len;
-        PersonArray = Arrays.copyOf(PersonArray, newlen);
+        personArray = Arrays.copyOf(personArray, personArray.length * 2);
     }
-
     // To display all the persons list
     public String toString() {
         StringBuilder sb = new StringBuilder("[");
         for (int i = 0; i < size - 1; ++i) {
-            sb.append(PersonArray[i] + ", \n");
+            sb.append(personArray[i] + ", \n");
         }
-        sb.append(PersonArray[size - 1] + "]");
+        sb.append(personArray[size - 1] + "]");
         return sb.toString();
     }
-
     // Return the size
     public int size() {
         return size;
@@ -84,9 +90,7 @@ public class MyPersonList {
         System.out.println("\nSize() : " + list.size() + "\n" + list);
         list.remove("Bruce");
         System.out.println("\nSize() : " + list.size() + "\n" + list);
-        System.out.println("\nSearching of Lermon: " + list.find("Lermon"));
-        System.out.println("\nSearching of Lermon: " + list.find("Joe")); //Joe is Last Name
-
+        System.out.println("\nSearching of Joe: " + list.find("Joe"));
         System.out.println(list.get(2));
     }
 }
